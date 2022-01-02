d56d27a08ee276ce60374d74f451bd13d531978b
DamianOrzechowski <72198238+DamianOrzechowski@users.noreply.github.com>
2022-01-02 18:26:16 +0100

    Added Customers controller with Index and Details views

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibAppOrzechowski.Models;
+using LibAppOrzechowski.ViewModels;
+
+namespace LibAppOrzechowski.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+
+        }
+        public IActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                return Content("User not found");
+            }
+
+            return View(customer);
+
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            
+
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Aneta Walczak" },
+                new Customer { Id = 2, Name = "Jakub Nowak"}
+
+
+
+
+            };
+        }
+
+    }
+}
