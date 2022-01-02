7b579e48829ff1ba1dce5f464a5ee718a419af2e
DamianOrzechowski <72198238+DamianOrzechowski@users.noreply.github.com>
2022-01-02 17:40:20 +0100

    Added Index for Book model

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -42,25 +42,27 @@ namespace LibAppOrzechowski.Controllers
             return Content("id=" + bookId);
         }
 
-        public IActionResult Index(int? pageIndex, string sortBy)
+        public IActionResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
+            var books = GetBooks();
 
-            if (String.IsNullOrEmpty(sortBy))
-            {
-                sortBy = "Name";
-            }
+            return View(books);
 
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
         }
 
         [Route(template:"books/released/{year:regex(^\\d{{4}}$)}/{month:range(1, 12)}")]
         public IActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book {Id = 1, Name = "Hamlet"},
+                new Book {Id = 2, Name = "Ulysses"}
+            };
         }
 
 
